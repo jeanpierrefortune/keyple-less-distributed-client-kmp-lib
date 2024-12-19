fb00c383c7c7c8c685e173d3819f461e1ddc1347
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2024-12-19 11:11:57 +0100

    chore: remove unused keyple and jaxb dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -37,10 +37,6 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.eclipse.keyple:keyple-gradle:0.+")
         classpath("com.diffplug.spotless:spotless-plugin-gradle:6.25.0")
-
-        classpath("javax.xml.bind:jaxb-api:2.3.1")
-        classpath("com.sun.xml.bind:jaxb-impl:2.3.9")
     }
 }
